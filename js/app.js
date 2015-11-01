97b6cf243cdc5ab28a358843c0df0dde903edf6b
YaronKoller <yaronkoller@live.ca>
2015-11-01 18:33:26 -0500

    Moved all html into index.html for shorter loading time

--- js/app.js
+++ js/app.js
@@ -1,5 +1,27 @@
 function main() {
+	$('.home-btn').click(function() {
+		$('.active-btn').removeClass('active-btn');
+		$(this).addClass('active-btn');
 
+		$('.show').removeClass('show');
+		$('.home').addClass('show');
+	});
+
+	$('.projects-btn').click(function() {
+		$('.active-btn').removeClass('active-btn');
+		$(this).addClass('active-btn');
+
+		$('.show').removeClass('show');
+		$('.projects').addClass('show');
+	});
+
+	$('.contact-btn').click(function() {
+		$('.active-btn').removeClass('active-btn');
+		$(this).addClass('active-btn');
+
+		$('.show').removeClass('show');
+		$('.contact').addClass('show');
+	});
 }
 
 $(document).ready(main);
