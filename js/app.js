8201408b280a95cce9a832f10f405945976035b6
YaronKoller <yaronkoller@live.ca>
2015-11-05 22:53:53 -0500

    Set up project carousel, fixed alignment in project tab, renamed some files/directories

--- js/app.js
+++ js/app.js
@@ -14,6 +14,34 @@ function main() {
 		$('.home').removeClass('show');
 		$('.projects').addClass('show');
 	});
+
+	$('.next-btn').click(function() {
+		var current = $('.project.show');
+		var next = current.next();
+
+		if (next.length === 0) {
+			next = $('.project').first();
+		}
+
+		current.fadeOut("fast", function() {
+			current.removeClass('show');
+			next.fadeIn("fast").addClass('show');
+		});
+	});
+
+	$('.prev-btn').click(function() {
+		var current = $('.project.show');
+		var prev = current.prev();
+
+		if (prev.length === 0) {
+			prev = $('.project').last();
+		}
+
+		current.fadeOut("fast", function() {
+			current.removeClass('show');
+			prev.fadeIn("fast").addClass('show');
+		});
+	});
 }
 
 $(document).ready(main);
